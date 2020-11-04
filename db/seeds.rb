1b405e4f01c024253771f9a9dbe134ed3f85d393
arbrown110 <arbrown110@gmail.com>
2020-11-04 04:06:56 -0500

    category model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+food = Category.create(name: "Food")
+vid = Category.create(name: "Media")
+clothes = Category.create(name: "Clothing")
+location = Category.create(name: "Travel")

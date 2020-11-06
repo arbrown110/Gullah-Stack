df099e21d0399ecc3f596c537273e474799a7f1f
arbrown110 <arbrown110@gmail.com>
2020-11-06 16:06:02 -0500

    seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,12 @@ vid = Category.create(name: "Media")
 clothes = Category.create(name: "Clothing")
 location = Category.create(name: "Travel")
 
+
+Collection.create(name:"slammin' sammen", image_url: "slammin_salmon.jpeg", description:
+"blackened atlantic salmon (6oz) topped wit' a crabby cream sauce (contains capers and spinach), a fried oyster & shrimp. served ova white rice & geechee fried corn", source_url: "https://virgilsgullahkitchen.com/menu", category_id: food.id )
+ 
+Collection.create(name:"geechee fried corn", image_url: "fried-corn-with-bacon-one.jpg", description:
+"sweet corn fried ina skillet wit' peppers, onions & gullah seasonings", source_url: "https://virgilsgullahkitchen.com/menu", category_id: food.id )
+ 
+Collection.create(name:"shrimp n' grits wit' crab", image_url: "shrimp_and_grits.jpg", description:
+"six (6) white shrimp cooked in crab gravy ova stone-ground chedda cheese grits (substitute grits for jazmine rice)", source_url: "https://virgilsgullahkitchen.com/menu", category_id: food.id )
